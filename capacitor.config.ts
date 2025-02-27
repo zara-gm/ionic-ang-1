ab604eacb3c10a8f24788433e57847d4d57a495e
zara-gm <43872052+zara-gm@users.noreply.github.com>
2025-02-26 17:30:32 -0800

    deleted-ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.updemy-course,ionic-ang',
+  appId: 'com.updemycourse.ionicang',
   appName: 'ionic-ang-course',
   webDir: 'www'
 };
