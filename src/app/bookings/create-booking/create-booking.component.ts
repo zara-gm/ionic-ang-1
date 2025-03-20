274aa35a4843cf93d4707b8522d0b21fc8910457
zara-gm <43872052+zara-gm@users.noreply.github.com>
2025-03-20 16:54:05 -0700

    create-booking

---
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Place } from '../../places/place.model';
+
+@Component({
+  selector: 'app-create-booking',
+  templateUrl: './create-booking.component.html',
+  styleUrls: ['./create-booking.component.scss'],
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class CreateBookingComponent implements OnInit {
+  @Input() selectedPlace: Place;
+
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() { }
+
+  onCancel() {
+    this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  onBookPlace() {
+    this.modalCtrl.dismiss({ message: 'This is a dummy message!' }, 'confirm');
+  }
+}
