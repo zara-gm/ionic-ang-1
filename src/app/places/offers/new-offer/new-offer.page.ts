5989874918b54e31c975451029f2ae3ea8709142
zara-gm <43872052+zara-gm@users.noreply.github.com>
2025-03-20 18:15:09 -0700

    new-offer-page

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
-  standalone: true,
   templateUrl: './new-offer.page.html',
   styleUrls: ['./new-offer.page.scss'],
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class NewOfferPage { }
+export class NewOfferPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
